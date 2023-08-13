31d5b8de51844f798d09af25911b5be29f9aceaa
Hasan <hsmusayev@gmail.com>
2023-08-13 20:20:58 +0400

    Added models and hospitalinfo service

---
+++ Hospital.Services/HospitalInfoService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Models;
+using Hospital.Repositories.Interfaces;
+using Hospital.Utilites;
+using Hospital.ViewModels;
+
+namespace Hospital.Services
+{
+    public class HospitalInfoService : IHospitalInfo
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public HospitalInfoService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public PagedResult<HospitalInfoViewModel> GetAll(int pageNumber, int pageSize)
+        {
+            var HospitalInfoViewModel = new HospitalInfoViewModel();
+        }
+    }
+}
---
+++ Hospital.Services/IHospitalInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Utilites;
+using Hospital.ViewModels;
+
+namespace Hospital.Services
+{
+    public interface IHospitalInfo
+    {
+        PagedResult<HospitalInfoViewModel> GetAll(int pageNumber, int pageSize);
+    }
+}
